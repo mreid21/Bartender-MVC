1199d6da46258261b9797fa65f4fabd6e6d0d86e
Matt <n01437978@unf.edu>
2022-08-26 19:01:17 -0400

    create and get bartender

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,14 +1,56 @@
 import { isBartender } from "../utils/typeguards";
 import { RequestHandler } from "express";
+import prisma  from '../prisma-init'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 
-const login: RequestHandler = (req, res) => {
+const login: RequestHandler = async (req, res) => {
+    const reqParams = {username: req.params.username, password: req.params.password}
+    const credentials = isBartender(reqParams) ? reqParams : undefined
+
+    if(credentials){
+        const {username, password} = credentials
+
+        try {
+            const bartender = await prisma.bartender.findUnique({where: {username}})
+
+            res.status(200).send(bartender)
+        }
+        catch(e: unknown) {
+            if(e instanceof PrismaClientKnownRequestError) {
+                res.status(500).send({message: `error: ${e.code}`})
+            }
+        }
+
+    }
+}
+
+const createBartender: RequestHandler = async (req, res) => {
     const credentials = isBartender(req.body) ? req.body : undefined
 
     if(credentials){
         const {username, password} = credentials
+
+        try {
+            const newBartender = await prisma.bartender.create({
+                data: {
+                    username,
+                    password
+                }
+            })
+
+            res.status(201).send({username: newBartender.username})
+        }
+        catch (e: unknown) {
+            if(e instanceof PrismaClientKnownRequestError && e.code === 'P2002'){
+                res.status(400).send({message: `bartender with username: ${username} already exists`})
+            }
+        }
+    }
+    else {
+        res.status(400).send({message: 'Invalid Request'})
     }
 }
 
 
-export {login}
+export {login, createBartender}
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { login } from "../controllers/userController";
+import { createBartender, login } from "../controllers/userController";
 
 const usersRoute = express.Router();
 
 usersRoute.get('/:username/:password', login)
+usersRoute.post('/', createBartender)
 
 
 export {usersRoute}

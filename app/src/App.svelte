6e5092a3702f9416584f987e13e4b4dbb25ba7c3
Matt <n01437978@unf.edu>
2022-08-28 02:10:01 -0400

    added images

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,86 +1,87 @@
 <script lang="ts">
 
-  import Cocktail from "./lib/Cocktail.svelte";
   import Menu from "./lib/Menu.svelte";
   let menu = [
     {
       id: 1, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 2, 
       name: "Super Screwdriver",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'everything-and-nothing.jpg', alt: ''}
     },
     {
       id: 3, 
       name: "Crazy Twist",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'lost-in-space-time.jpg', alt: ''}
     },
     {
       id: 4, 
       name: "Lost in Space-Time",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'subzero.jpg', alt: ''}
     },
     {
       id: 5, 
       name: "Everything and Nothing",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 6, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 7, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 8, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 9, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 10, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 11, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
     {
       id: 12, 
       name: "Cosmic Sunset",
       price: 200,
-      img: {src: 'test-cocktail.jpg', alt: ''}
+      img: {src: 'cosmic-sunset.jpg', alt: ''}
     },
 
 
   ]
 </script>
 
+
 <main class="w-screen flex justify-center">
+  
   <div class="container">
     <Menu menu={menu}/>
   </div>

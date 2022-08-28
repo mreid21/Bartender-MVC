2dc9e4b92546160d1e5a20bd607459d4ec1dc89a
Matt <n01437978@unf.edu>
2022-08-28 17:27:12 -0400

    order total calculates based on cart

--- app/src/lib/Checkout.svelte
+++ app/src/lib/Checkout.svelte
@@ -3,7 +3,7 @@
   import OrderItem from '../lib/OrderItem.svelte'
   import { Navigate } from 'svelte-router-spa'
   let cartItems = []
-
+  $: total = cartItems.map(item => item.cocktail.price * item.quantity).reduce((prev, next) => prev + next, 0)
   cart.subscribe(cartData => {
     cartItems = cartData
   })
@@ -17,7 +17,7 @@
           <OrderItem item={item}/>  
       {/each}
       <div class="flex flex-col items-end px-4">
-        <p class="text-2xl mb-4">Total: <span class="text-3xl font-semibold">$1080</span></p>
+        <p class="text-2xl mb-4">Total: <span class="text-3xl font-semibold">${total}</span></p>
         <div>
           <span class="inline-block px-4 py-2 mr-2 border rounded-lg hover:bg-slate-600 hover:text-white transition">
             <Navigate to="/">Go back</Navigate>

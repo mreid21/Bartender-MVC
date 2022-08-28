2777908b98af404be15b4f42366627d64cc00220
Matt <n01437978@unf.edu>
2022-08-28 17:14:22 -0400

    styled checkout

--- app/src/lib/Checkout.svelte
+++ app/src/lib/Checkout.svelte
@@ -1,12 +1,62 @@
 <script lang="ts">
   import {cart} from '../store/store'
+  import OrderItem from '../lib/OrderItem.svelte'
   let cartItems = []
 
   cart.subscribe(cartData => {
     cartItems = cartData
   })
 </script>
-<div>
-  <p>hello from checkout</p>
-  <p>{JSON.stringify(cartItems)}</p>
-</div>
+
+
+<section class="w-screen lg:flex lg:justify-center">
+  <div class="container">
+    <div class="flex flex-col gap-4 min-w-full">
+      <OrderItem/>
+      <OrderItem/>
+      <OrderItem/>
+      <OrderItem/>
+      <div class="flex flex-col items-end px-4">
+        <p class="text-2xl mb-4">Total: <span class="text-3xl font-semibold">$1080</span></p>
+        <div>
+          <button class="inline-block px-4 py-2 mr-2 border rounded-lg hover:bg-slate-600 hover:text-white transition">Go back</button>
+          <button class="button-36">Checkout</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<style>
+
+  /* CSS */
+  
+  
+  /* CSS */
+  .button-36 {
+    background-image: linear-gradient(92.88deg, #455EB5 9.16%, #5643CC 43.89%, #673FD7 64.72%);
+    border-radius: 8px;
+    border-style: none;
+    box-sizing: border-box;
+    color: #FFFFFF;
+    cursor: pointer;
+    flex-shrink: 0;
+    font-family: "Inter UI","SF Pro Display",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    height: 2.5rem;
+    padding: 0 1.6rem;
+    text-align: center;
+    text-shadow: rgba(0, 0, 0, 0.25) 0 3px 8px;
+    transition: all .5s;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+  }
+  
+  .button-36:hover {
+    box-shadow: rgba(80, 63, 205, 0.5) 0 1px 30px;
+    transition-duration: .1s;
+  }
+  </style>
---
+++ app/src/lib/OrderItem.svelte
@@ -0,0 +1,28 @@
+<section class="flex gap-4 flex-start w-full p-4 h-48  h-48 border-b-2">
+  <div class="w-32 basis-1/2 lg:basis-1/4">
+    <img class="rounded-sm w-full h-full object-cover" src={'../../public/images/deep-blue.jpg'} alt="test">
+  </div>
+  <div class="flex justify-between basis-full">
+    <div class="flex flex-col justify-between">
+      <div>
+        <span>#1</span>
+        <p class="text-lg font-bold">Deep Blue</p>
+      </div>
+      <div>
+        <span class="underline">remove</span>
+      </div>
+    </div>
+    <div class="flex flex-col justify-between">
+      <p class="text-lg font-bold">$90</p>
+      <div class="flex items-center gap-2">
+        <button class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-8 w-8 cursor-pointer">
+          <span class="m-auto text-2xl font-thin">-</span>
+        </button>
+        <span>0</span>
+        <button class="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-8 w-8 cursor-pointer">
+          <span class="m-auto text-xl font-thin">+</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</section>

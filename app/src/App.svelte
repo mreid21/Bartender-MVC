0c9841d1bdf298b6eabce5b4a793c1f5c628bed8
Matt <n01437978@unf.edu>
2022-08-27 21:48:11 -0400

    updated cocktail component

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
 
   import Cocktail from "./lib/Cocktail.svelte";
-
+  let cocktail = {
+    id: 1, 
+    name: "Cosmic Sunset",
+    price: 50,
+    img: {src: 'test-cocktail.jpg', alt: ''}
+  }
 </script>
 
-<main class="h-screen w-screen flex justify-center items-center">
-  <Cocktail cocktail={{id: 1, name: "Cosmic Sunset", price: 50, img: {src: 'test-cocktail.jpg', alt: ''}}}/>
+<main>
+  <div class="grid place-items-center max-w-lg gap-8 md:grid-cols-2">
+    <Cocktail cocktail={cocktail}/>
+    <Cocktail cocktail={cocktail}/>
+    <Cocktail cocktail={cocktail}/>
+    <Cocktail cocktail={cocktail}/>
+    <Cocktail cocktail={cocktail}/>
+  </div>
 </main>
 
 <style global>
